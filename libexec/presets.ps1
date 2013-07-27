39f3b67a352c4579fdd753f1f58a1858025abc6b
Luke Sampson <me@lukesampson.com>
2013-07-27 21:11:01 +1000

    add presets command to list available presets

---
+++ libexec/presets.ps1
@@ -0,0 +1,8 @@
+# Usage: concfg presets
+# Summary: List available presets
+# Help: Lists the presets that are available for the 'concfg import' command
+
+"Available presets:"
+gci "$psscriptroot\..\presets" | % {
+    "   $([io.path]::getfilenamewithoutextension($_.name))"
+}
