0d13e207d3a7ab1116c2beb69473e4de3935aecc
Anant Anand Gputa <anantanandgupta@gmail.com>
2016-11-14 22:24:49 +0530

    concfg site started

--- libexec/presets.ps1
+++
@@ -1,8 +0,0 @@
-# Usage: concfg presets
-# Summary: List available presets
-# Help: Lists the presets that are available for the 'concfg import' command
-
-"Available presets:"
-gci "$psscriptroot\..\presets" | % {
-	"   $([io.path]::getfilenamewithoutextension($_.name))"
-}
