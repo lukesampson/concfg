7c290c335822bcf7baec1c603ece1903a15b2f59
Troy Harrison <ibigpapa@gmail.com>
2016-11-16 12:57:58 -0600

    Screenshots
    Screenshots

---
+++ preset_examples/theme-output-comparison.ps1
@@ -0,0 +1,18 @@
+﻿$conCfgPresetsOutput = concfg presets
+
+$conCfgPresetOutputArr = $conCfgPresets -split " "
+
+$conCfgPresetsArr = [System.Collections.ArrayList] @()
+
+foreach ($word in $conCfgPresetOutputArr){
+    if ($word -inotlike "" -and $word -inotlike "Available" -and $word -inotlike "presets:"){
+        $conCfgPresetsArr.Add($word)
+    }
+}
+
+foreach ($preset in $conCfgPresetsArr){
+    concfg clean
+    concfg import -n $preset
+    start Powershell "-NoExit -File .\outcolors.ps1 -preset $preset"
+    sleep -Seconds 3
+}
