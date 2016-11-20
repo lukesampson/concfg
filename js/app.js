3681224668505007327a146d2865ecc6f1025b1c
Anant Anand Gputa <anantanandgupta@gmail.com>
2016-11-20 19:00:09 +0530

    Added edit preset feature

--- js/app.js
+++ js/app.js
@@ -1,6 +1,64 @@
 (function() {
     'use strict';
     
-    angular.module('mainApp', ['ui.bootstrap']);
-    
+    angular.module('mainApp', ['ui.bootstrap'])
+        .run(function() {
+            var imageObj = new Image();
+            imageObj.onload = function() {
+                init(this);
+            };
+            imageObj.src = 'assets/img/rgb.png';
+
+            function getMousePos(canvas, evt) {
+                var rect = canvas.getBoundingClientRect();
+                return {
+                    x: Math.round((evt.clientX - rect.left) * 2400 / rect.width),
+                    y: Math.round((evt.clientY - rect.top) * 960 / rect.height)
+                };
+            }
+
+            function init(imageObj) {
+                var canvas = document.getElementById('colorPicker');
+                var context = canvas.getContext('2d');
+
+                // canvas.addEventListener('mousedown', function() {
+                // mouseDown = true;
+                // }, false);
+
+                // canvas.addEventListener('mouseup', function() {
+                // mouseDown = false;
+                // }, false);
+
+                canvas.addEventListener('click', function(evt) {
+                    var cordinates = getMousePos(canvas, evt);
+                    var imageData = context.getImageData(cordinates.x, cordinates.y, 1, 1);
+                    var data = imageData.data;
+                    var colorHex = '#' +
+                        ("00" + data[0].toString(16)).slice(-2) + 
+                        ("00" + data[1].toString(16)).slice(-2) + 
+                        ("00" + data[2].toString(16)).slice(-2);
+                        $('#colorInput').val(colorHex);
+                        $('#colorInput').trigger('input');
+                }, false);
+
+                canvas.addEventListener('mousemove', function(evt) {
+                    // var mousePos = getMousePos(canvas, evt);
+                    // var color = undefined;
+
+                    // if(mouseDown && mousePos !== null) {
+                    //     var imageData = context.getImageData(0, 0, mousePos.x + 1, mousePos.y + 1);
+                    //     var data = imageData.data;
+                    //     var x = mousePos.x;
+                    //     var y = mousePos.y;
+                    //     var red = data[mousePos.x * (y + 1) * 4];
+                    //     var green = data[mousePos.x * (y + 1) * 4 + 1];
+                    //     var blue = data[mousePos.x * (y + 1) * 4 + 2];
+                    //     //var color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+                    //     console.log(red, green, blue);
+                    // }
+                }, false);
+
+                context.drawImage(imageObj, 0, 0);
+            }
+        });
 })();
--- js/app.main.controller.js
+++ js/app.main.controller.js
@@ -3,6 +3,73 @@
 
     angular.module('mainApp')
         .controller('mainController', ['$scope', 'gitService', function ($scope, git) {
+            
+            var colorKeys = [
+                {
+                    'key':'black', 
+                    'displayName':'Black'
+                }, 
+                {
+                    'key':'white', 
+                    'displayName':'White'
+                }, 
+                {
+                    'key':'dark_blue', 
+                    'displayName':'Dark Blue'
+                }, 
+                {
+                    'key':'blue', 
+                    'displayName':'Blue'
+                }, 
+                {
+                    'key':'dark_green', 
+                    'displayName':'Dark Green'
+                }, 
+                {
+                    'key':'green', 
+                    'displayName':'Green'
+                }, 
+                {
+                    'key':'dark_cyan', 
+                    'displayName':'Dark Cyan'
+                }, 
+                {
+                    'key':'cyan', 
+                    'displayName':'Cyan'
+                }, 
+                {
+                    'key':'dark_red', 
+                    'displayName':'Dark Red'
+                }, 
+                {
+                    'key':'red', 
+                    'displayName':'Red'
+                }, 
+                {
+                    'key':'dark_magenta', 
+                    'displayName':'Dark Magenta'
+                }, 
+                {
+                    'key':'magenta', 
+                    'displayName':'Magenta'
+                }, 
+                {
+                    'key':'dark_yellow', 
+                    'displayName':'Dark Yellow'
+                }, 
+                {
+                    'key':'yellow', 
+                    'displayName':'Yellow'
+                }, 
+                {
+                    'key':'dark_gray', 
+                    'displayName':'Dark Gray'
+                }, 
+                {
+                    'key':'gray', 
+                    'displayName':'Gray'
+                }];
+            
             var defaultPreset = {
                 "black":        "#000000",
                 "dark_blue":    "#000080",
@@ -22,36 +89,47 @@
                 "white":        "#ffffff"
             }
 
-            $scope.preset = {};
+            $scope.colorKeys = colorKeys;
 
+            $scope.preset = {};
+            $scope.presetEdited = {};
             $scope.defaultPreset = {};
             
             angular.copy(defaultPreset, $scope.preset);
+            angular.copy(defaultPreset, $scope.presetEdited);
             angular.copy(defaultPreset, $scope.defaultPreset);
 
             $scope.hasColors = true;
-        
+            $scope.isEdited = false;
+
             $scope.status = {
                 isopen: false
             };
 
-            $scope.isTemplateSelected = false;
-
             $scope.selectedPreset = null;
             $scope.selectedPresetPath = 'Select Preset';
+            $scope.isTemplateSelected = false;
 
+            // change handler for the dropdown menu
             $scope.presetSelected = function(preset) {
                 $scope.selectedPreset = preset;
                 $scope.selectedPresetPath = preset.path;
                 $scope.isTemplateSelected = true;
             }
 
+            // click handler for load action button
             $scope.load = function() {
                 git.getPreset($scope.selectedPreset.url).then(function (content) {
                     var decodedContent = atob(content);
                     console.log('retrieved preset ' + $scope.selectedPreset.path);
                     console.log(decodedContent);
                     $scope.preset = JSON.parse(decodedContent);
+
+                    // load the selectedPreset into the edited preset if there are no edits
+                    if(!$scope.isEdited) {
+                        angular.copy($scope.preset, $scope.presetEdited);
+                    }
+
                     $scope.hasColors =  $scope.preset.black || 
                                         $scope.preset.dark_blue ||
                                         $scope.preset.dark_green ||
@@ -72,26 +150,48 @@
                 });
             }
 
-            var invertColor = function (hexTripletColor) {
-                var color = hexTripletColor;
-                color = color.substring(1);           // remove #
-                color = parseInt(color, 16);          // convert to integer
-                color = 0xFFFFFF ^ color;             // invert three bytes
-                color = color.toString(16);           // convert to hex
-                color = ("000000" + color).slice(-6); // pad with leading zeros
-                color = "#" + color;                  // prepend #
-                return color;
+            // var invertColor = function (hexTripletColor) {
+            //     var color = hexTripletColor;
+            //     color = color.substring(1);           // remove #
+            //     color = parseInt(color, 16);          // convert to integer
+            //     color = 0xFFFFFF ^ color;             // invert three bytes
+            //     color = color.toString(16);           // convert to hex
+            //     color = ("000000" + color).slice(-6); // pad with leading zeros
+            //     color = "#" + color;                  // prepend #
+            //     return color;
+            // }
+
+            // $scope.invertColor = invertColor;
+
+            $scope.colorToEdit = null;
+            // click handler for edit action button
+            $scope.editColor = function(colorKey) {
+                $scope.colorKey = colorKey;
+                $scope.colorToEdit = $scope.presetEdited[colorKey.key];
+
+                // var colorHex = $scope.presetEdited[colorKey.key];
+                // var red = colorHex.substring(1).substring(0,2);
+                // var green = colorHex.substring(1).substring(2,4);
+                // var blue = colorHex.substring(1).substring(4,6);
+                // console.log(red,green,blue);
             }
 
-            $scope.invertColor = invertColor;
-
-            $scope.editColor = function(colorHex) {
-                var red = colorHex.substring(1).substring(0,2);
-                var green = colorHex.substring(1).substring(2,4);
-                var blue = colorHex.substring(1).substring(4,6);
-                console.log(red,green,blue);
+            $scope.resetEdits = function() {
+                angular.copy($scope.preset, $scope.presetEdited);
+                $scope.isEdited = false;
             }
 
+            $scope.editDone = function() {
+                $scope.presetEdited[$scope.colorKey.key] = $scope.colorToEdit;
+                $scope.colorToEdit = null;
+                $scope.isEdited = true;
+            }
+
+            $scope.editCancel = function() {
+                $scope.colorToEdit = null;
+            }
+
+            // get the border color for the edit action button
             $scope.borderColor = function(colorHex) {
                 var red = parseInt(colorHex.substring(1).substring(0,2), 16);
                 var green = parseInt(colorHex.substring(1).substring(2,4), 16);
@@ -99,6 +199,7 @@
                 return red > 0x80 || green > 0x80 || blue > 0x80 ? (red === 0x00 && green === 0x00 || red === 0x00 && blue === 0x00 || green === 0x00 && blue === 0x00) ? "white" : "black" : "white";
             }
 
+            // the list of presets from the master branch
             git.getBranch().then(function(response) {
                 console.log('retrieved branch');
                 git.getBranchTree().then(function (response) {
