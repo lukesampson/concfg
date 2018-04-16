9b182f837837c5b1434fbadbb6f8670038de77b0
Todd Knarr <tknarr@silverglass.org>
2018-04-15 20:22:47 -0700

    Add script for syntax coloring

---
+++ bin/syntax-coloring.ps1
@@ -0,0 +1,13 @@
+# Set syntax highlighting colors to match Base16 conventions
+#
+Set-PSReadlineOption -TokenKind Comment -ForegroundColor DarkCyan
+Set-PSReadlineOption -TokenKind Operator -ForegroundColor DarkMagenta
+Set-PSReadlineOption -TokenKind Variable -ForegroundColor DarkGray
+Set-PSReadlineOption -TokenKind Member -ForegroundColor DarkGray
+Set-PSReadlineOption -TokenKind Number -ForegroundColor Blue
+Set-PSReadlineOption -TokenKind Type -ForegroundColor Green
+Set-PSReadlineOption -TokenKind String -ForegroundColor Cyan
+Set-PSReadlineOption -TokenKind Parameter -ForegroundColor Red
+Set-PSReadlineOption -TokenKind Keyword -ForegroundColor Yellow
+Set-PSReadlineOption -TokenKind Command -ForegroundColor White
+Set-PSReadlineOption -TokenKind None -ForegroundColor DarkMagenta
